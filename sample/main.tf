cf79bdb414aff44e7b3cd3760e7244efbdcdabba
sidmen <sidharth.rulez@gmail.com>
2017-09-29 20:26:01 +0530

    terraform modifications

--- sample/main.tf
+++ sample/main.tf
@@ -5,6 +5,13 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami = "ami-2d39803a"
   instance_type = "t2.micro"
+  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+  
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p "${var.server_port}" &
+              EOF
 
   tags {
    Name = "terraform-example"
@@ -15,9 +22,13 @@ resource "aws_instance" "example" {
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
   ingress {
-    from_port = 8080
-    to_port = 8080
+    from_port = "${var.server_port}"
+    to_port = "${var.server_port}"
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+output "public_ip" {
+  value = "${aws_instance.example.public_ip}"
 }
